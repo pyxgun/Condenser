50f1f275aaffec3e3e0a6f833c725ec969383d37
jixeis9541@gmail.com <jixeis9541@gmail.com>
2026-01-22 15:54:43 +0900

    implement iptables chain in bootstrap

--- internal/env/bootstrap.go
+++ internal/env/bootstrap.go
@@ -48,27 +48,32 @@ func (m *BootstrapManager) SetupRuntime() error {
 		return err
 	}
 
-	// 4. setup IPAM (IP Address Managemant)
+	// 3. setup IPAM (IP Address Managemant)
 	if err := m.setupIpam(); err != nil {
 		return err
 	}
 
-	// 5. setup CSM (Container State Management)
+	// 4. setup CSM (Container State Management)
 	if err := m.setupCsm(); err != nil {
 		return err
 	}
 
-	// 6. setup ILM (Image Layer Management)
+	// 5. setup ILM (Image Layer Management)
 	if err := m.setupIlm(); err != nil {
 		return err
 	}
 
-	// 3. setup network
+	// 6. setup network
 	if err := m.setupNetwork(); err != nil {
 		return err
 	}
 
-	// 7. setup AppArmor
+	// 7. setup network chain
+	if err := m.setupNetworkChain(); err != nil {
+		return err
+	}
+
+	// 8. setup AppArmor
 	if err := m.setupAppArmor(); err != nil {
 		return err
 	}
@@ -175,6 +180,26 @@ func (m *BootstrapManager) writeCgroupController(token string) error {
 	return nil
 }
 
+func (m *BootstrapManager) setupIpam() error {
+	return m.ipamStoreHandler.SetConfig()
+}
+
+func (m *BootstrapManager) setupCsm() error {
+	return m.csmStoreHandler.SetContainerState()
+}
+
+func (m *BootstrapManager) setupIlm() error {
+	return m.ilmStoreHandler.SetConfig()
+}
+
+func (m *BootstrapManager) setupAppArmor() error {
+	if err := m.appArmorHandler.EnsureRaindDefaultProfile(); err != nil {
+		// if apparmor setting failed, runtime ignore apparmor setting
+		return nil
+	}
+	return nil
+}
+
 func (m *BootstrapManager) setupNetwork() error {
 	// 1. create bridge interface
 	if err := m.createBridgeInterface(); err != nil {
@@ -190,6 +215,8 @@ func (m *BootstrapManager) setupNetwork() error {
 	if err := m.createManagementProtectRule(); err != nil {
 		return err
 	}
+
+	// 4. setup chain
 
 	return nil
 }
@@ -267,22 +294,288 @@ func (m *BootstrapManager) createManagementProtectRule() error {
 	return nil
 }
 
-func (m *BootstrapManager) setupIpam() error {
-	return m.ipamStoreHandler.SetConfig()
-}
+func (m *BootstrapManager) setupNetworkChain() error {
+	// raind default chains
+	// 1. RAIND-ROOT
+	//      manage all Raind Rules
+	// 2. RAIND-EW
+	//      manage East-West(contaier-to-container) traffic rules
+	// 3. RAIND-NS-OBS
+	//      manage North-West(container-to-external) traffic rules
+	//      this chain is all accept and log traffic default
+	// 4. RAIND-NS-ENF
+	//      manage North-West(container-to-external) traffic rules
+	//      this chain is all denay and allow explicit
 
-func (m *BootstrapManager) setupCsm() error {
-	return m.csmStoreHandler.SetContainerState()
-}
-
-func (m *BootstrapManager) setupIlm() error {
-	return m.ilmStoreHandler.SetConfig()
-}
-
-func (m *BootstrapManager) setupAppArmor() error {
-	if err := m.appArmorHandler.EnsureRaindDefaultProfile(); err != nil {
-		// if apparmor setting failed, runtime ignore apparmor setting
-		return nil
+	// == setup predefined rules ==//
+	networkList, err := m.ipamHandler.GetNetworkList()
+	if err != nil {
+		return err
 	}
+
+	// TODO: implement Observe/Enforce mode
+	enforce := false
+	// TODO: implement Nflog mode
+	nflog := true
+
+	// 1. create chain
+	if err := m.createChain(); err != nil {
+		return err
+	}
+
+	// 2. insert chain to forward
+	if err := m.insertForward(); err != nil {
+		return err
+	}
+
+	// 3. build RAIND-ROOT chain
+	if err := m.buildRaindRootChain(enforce); err != nil {
+		return err
+	}
+
+	// 4. build RAIND-EW chain
+	if err := m.buildRaindEWChain(networkList, nflog); err != nil {
+		return err
+	}
+
+	// 5. build RAIND-NS-OBS chain
+	if err := m.buildRaindNSObserveChain(networkList, nflog); err != nil {
+		return err
+	}
+
+	// 6. build RAIND-NS-ENF chain
+	if err := m.buildRaindNSEnforceChain(networkList, nflog); err != nil {
+		return err
+	}
+
+	// == setup user-defined rules ==
+
+	return nil
+}
+
+func (m *BootstrapManager) createChain() error {
+	chains := []string{
+		"RAIND-ROOT",
+		"RAIND-EW",
+		"RAIND-NS-OBS",
+		"RAIND-NS-ENF",
+	}
+	for _, c := range chains {
+		if err := m.networkHandler.CreateChain(c); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (m *BootstrapManager) insertForward() error {
+	if err := m.networkHandler.InsertForwardRule("RAIND-ROOT"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *BootstrapManager) buildRaindRootChain(enforce bool) error {
+	chainName := "RAIND-ROOT"
+	// 1. allow return traffic (ESTABLISHED,RELATED)
+	if err := m.networkHandler.AddRuleToChain(
+		chainName,
+		network.RuleModel{
+			Conntrack: true,
+			Ctstate:   []string{"ESTABLISHED", "RELATED"},
+		},
+		"ACCEPT",
+	); err != nil {
+		return err
+	}
+
+	// 2. forward to RAIND-EW
+	if err := m.networkHandler.AddRuleToChain(
+		chainName,
+		network.RuleModel{},
+		"RAIND-EW",
+	); err != nil {
+		return err
+	}
+
+	if enforce {
+		// TODO: implement Observe/Enforce mode
+	} else {
+		// 3. forward  to RAIND-NS-OBS
+		if err := m.networkHandler.AddRuleToChain(
+			chainName,
+			network.RuleModel{},
+			"RAIND-NS-OBS",
+		); err != nil {
+			return err
+		}
+	}
+
+	// 4. other: return
+	if err := m.networkHandler.AddRuleToChain(
+		chainName,
+		network.RuleModel{},
+		"RETURN",
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *BootstrapManager) buildRaindEWChain(networkList []ipam.NetworkList, nflog bool) error {
+	chainName := "RAIND-EW"
+
+	// 1. if NFLOG mode is enabled, add NFLOG entry
+	if nflog {
+		for _, n := range networkList {
+			if err := m.networkHandler.AddRuleToChain(
+				chainName,
+				network.RuleModel{
+					Conntrack:       true,
+					Ctstate:         []string{"NEW"},
+					Physdev:         true,
+					PhysdevIsBridge: true,
+					InputDev:        n.Interface,
+					OutputDev:       n.Interface,
+					NflogGroup:      10,
+					NflogPrefix:     "RAIND-EW-DENY",
+				},
+				"NFLOG",
+			); err != nil {
+				return err
+			}
+		}
+	}
+
+	// 2. deny all inter-container traffic
+	for _, n := range networkList {
+		if err := m.networkHandler.AddRuleToChain(
+			chainName,
+			network.RuleModel{
+				InputDev:  n.Interface,
+				OutputDev: n.Interface,
+			},
+			"DROP",
+		); err != nil {
+			return err
+		}
+	}
+
+	// 3. other: return
+	if err := m.networkHandler.AddRuleToChain(
+		chainName,
+		network.RuleModel{},
+		"RETURN",
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *BootstrapManager) buildRaindNSObserveChain(networkList []ipam.NetworkList, nflog bool) error {
+	chainName := "RAIND-NS-OBS"
+	hostInterface, err := m.ipamHandler.GetDefaultInterface()
+	if err != nil {
+		return err
+	}
+
+	// 1. if NFLOG mode is enabled, add NFLOG entry
+	if nflog {
+		for _, n := range networkList {
+			if err := m.networkHandler.AddRuleToChain(
+				chainName,
+				network.RuleModel{
+					Conntrack:   true,
+					Ctstate:     []string{"NEW"},
+					InputDev:    n.Interface,
+					OutputDev:   hostInterface,
+					NflogGroup:  11,
+					NflogPrefix: "RAIND-NS-OBS",
+				},
+				"NFLOG",
+			); err != nil {
+				return err
+			}
+		}
+	}
+
+	// 2. allow container to external traffic
+	for _, n := range networkList {
+		if err := m.networkHandler.AddRuleToChain(
+			chainName,
+			network.RuleModel{
+				InputDev:  n.Interface,
+				OutputDev: hostInterface,
+			},
+			"ACCEPT",
+		); err != nil {
+			return err
+		}
+	}
+
+	// 3. other: return
+	if err := m.networkHandler.AddRuleToChain(
+		chainName,
+		network.RuleModel{},
+		"RETURN",
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *BootstrapManager) buildRaindNSEnforceChain(networkList []ipam.NetworkList, nflog bool) error {
+	chainName := "RAIND-NS-ENF"
+	hostInterface, err := m.ipamHandler.GetDefaultInterface()
+	if err != nil {
+		return err
+	}
+
+	// 1. if NFLOG mode is enabled, add NFLOG entry
+	if nflog {
+		for _, n := range networkList {
+			if err := m.networkHandler.AddRuleToChain(
+				chainName,
+				network.RuleModel{
+					Conntrack:   true,
+					Ctstate:     []string{"NEW"},
+					InputDev:    n.Interface,
+					OutputDev:   hostInterface,
+					NflogGroup:  12,
+					NflogPrefix: "RAIND-NS-ENF",
+				},
+				"NFLOG",
+			); err != nil {
+				return err
+			}
+		}
+	}
+
+	// 2. allow container to external traffic
+	for _, n := range networkList {
+		if err := m.networkHandler.AddRuleToChain(
+			chainName,
+			network.RuleModel{
+				InputDev:  n.Interface,
+				OutputDev: hostInterface,
+			},
+			"DROP",
+		); err != nil {
+			return err
+		}
+	}
+
+	// 3. other: return
+	if err := m.networkHandler.AddRuleToChain(
+		chainName,
+		network.RuleModel{},
+		"RETURN",
+	); err != nil {
+		return err
+	}
+
 	return nil
 }
